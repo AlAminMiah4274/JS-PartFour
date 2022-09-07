444f56f33eabf8a7376ef41b0ae982bc1a23f6c8
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-07 19:58:07 +0600

    shopping cat done

---
+++ simple-cart.js
@@ -0,0 +1,19 @@
+const shoppingCart = [
+    { name: 'shirt', price: 2500 },
+    { name: 'shoe', price: 3000 },
+    { name: 'tShirt', price: 1000 },
+    { name: 'pant', price: 1800 },
+    { name: 'belt', price: 1100 }
+];
+
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        sum = sum + product.price;
+    }
+    return sum;
+}
+
+const expence = totalCost(shoppingCart);
+console.log('Total price of today expence:', expence);
