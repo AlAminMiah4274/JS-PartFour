0dca11e3277bd78bd66bfdd1c5d6cdbda3718f72
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-07 23:24:43 +0600

    not complete

---
+++ discount.js
@@ -0,0 +1,13 @@
+/*
+1. First 100 -----> 100tk
+2. Second 100 ----> 90tk
+3. Rest ----------> 70tk
+*/
+
+function ticketPrice(ticketQuantity) {
+    const first100Rate = 100;
+    const second100Rate = 90;
+    const restTicketRate = 70;
+
+
+}
