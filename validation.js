933bbf9522e2e857a3eab689688bfbf49a8135ae
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-09 16:14:12 +0600

    validation done

---
+++ validation.js
@@ -0,0 +1,19 @@
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+// const result = add(15, 54);
+// console.log('The sum is:', result);
+
+// ********* //
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+// const result = multiply(3, 2);
+// console.log('Multiplication is:', result);
+
+let sum = 0;
+for (let i = 0; i <= 3; i++) {
+    sum = sum + i;
+    console.log(sum);
+}
