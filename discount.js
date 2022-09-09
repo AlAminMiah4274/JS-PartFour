4f4e613fb47eff5bf057c0f1e6198c8c908c591f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-09 11:39:38 +0600

    discount done

--- discount.js
+++ discount.js
@@ -9,5 +9,26 @@ function ticketPrice(ticketQuantity) {
     const second100Rate = 90;
     const restTicketRate = 70;
 
-
+    if (ticketQuantity <= 100) {
+        const price = ticketQuantity * first100Rate;
+        return price;
+    }
+    else if (ticketQuantity <= 200) {
+        const first100TicketPrice = 100 * first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTicketPrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100TicketPrice + restTicketPrice;
+        return totalPrice;
+    }
+    else {
+        const first100TicketPrice = 100 * first100Rate;
+        const Second100TicketPrice = 100 * second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTicketPrice = restTicketQuantity * restTicketRate;
+        const totalCost = first100TicketPrice + Second100TicketPrice + restTicketPrice;
+        return totalCost;
+    }
 }
+
+const prices = ticketPrice(230);
+console.log('Tickets price are:', prices);
