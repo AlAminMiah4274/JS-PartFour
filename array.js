e1e452f6bd3a820ddd9a422977f10cd1faa41c5e
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-17 23:31:42 +0600

    not done yet

---
+++ array.js
@@ -0,0 +1,14 @@
+// const products = [
+//     { product: 'phone', price: 18000 },
+//     { product: 'laptop', price: 35000 }
+// ];
+// products[0];
+
+// // console.log(products);
+
+const products = {
+    '0': 25,
+    '1': 50,
+    '2': 80
+}
+console.log(products);
---
+++ matched.js
@@ -0,0 +1,27 @@
+// const numbers = [15, 45, 78, 58, 51];
+// // for (let i = 0; i < numbers.length; i++) {
+// //     const number = numbers[i];
+// //     console.log(number);
+// // }
+
+// for (const number of numbers) {
+//     console.log(number);
+// }
+
+const products = [
+    { id: 1, name: 'xaomi', price: 19000 },
+    { id: 2, name: 'samsung', price: 22000 },
+    { id: 3, name: 'iphone', price: 119000 },
+    { id: 4, name: 'vivo', price: 15000 },
+    { id: 5, name: 'nokia', price: 35000 },
+    { id: 6, name: 'google pixel', price: 85000 },
+    { id: 7, name: 'oppo', price: 20000 },
+    { id: 8, name: 'walton', price: 13000 },
+];
+// for (const product of products) {
+//     console.log(product);
+// }
+
+function matchedProducts(products, search) {
+
+}
