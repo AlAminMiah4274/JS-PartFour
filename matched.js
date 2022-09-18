cce3f6c09619cbc207958d70271d58c09abacd55
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-18 16:33:44 +0600

    object done

--- matched.js
+++ matched.js
@@ -8,20 +8,54 @@
 //     console.log(number);
 // }
 
-const products = [
-    { id: 1, name: 'xaomi', price: 19000 },
-    { id: 2, name: 'samsung', price: 22000 },
-    { id: 3, name: 'iphone', price: 119000 },
-    { id: 4, name: 'vivo', price: 15000 },
-    { id: 5, name: 'nokia', price: 35000 },
-    { id: 6, name: 'google pixel', price: 85000 },
-    { id: 7, name: 'oppo', price: 20000 },
-    { id: 8, name: 'walton', price: 13000 },
-];
-// for (const product of products) {
-//     console.log(product);
+// const products = [
+//     { id: 1, name: 'xaomi phone one night', price: 19000 },
+//     { id: 2, name: 'mac book air', price: 22000 },
+//     { id: 3, name: 'iphone', price: 119000 },
+//     { id: 4, name: 'lenevo yoga laptop 2025', price: 15000 },
+//     { id: 5, name: 'dell inspiron laptop', price: 35000 },
+//     { id: 6, name: 'samsung Phone note 7', price: 85000 },
+//     { id: 7, name: 'nokia old age Phone gone', price: 20000 },
+//     { id: 8, name: 'Phone one', price: 13000 },
+//     { id: 8, name: 'M1 chip not chip LapTop', price: 13000 }
+// ];
+// // for (const product of products) {
+// //     console.log(product);
+// // }
+
+// function matchedProducts(products, search) {
+//     let matched = [];
+//     for (const product of products) {
+//         if (product.name.toLowerCase().includes(search.toLowerCase())) {
+//             matched.push(product);
+//         }
+//     }
+//     return matched;
 // }
+// const result = matchedProducts(products, 'laptop');
+// console.log(result);
 
+// ****************** //
+
+const products = [
+    { id: 1, name: 'xaomi phone one night', price: 19000 },
+    { id: 2, name: 'mac book air', price: 22000 },
+    { id: 3, name: 'iphone', price: 119000 },
+    { id: 4, name: 'lenevo yoga laptop 2025', price: 15000 },
+    { id: 5, name: 'dell inspiron laptop', price: 35000 },
+    { id: 6, name: 'samsung Phone note 7', price: 85000 },
+    { id: 7, name: 'nokia old age Phone gone', price: 20000 },
+    { id: 8, name: 'Phone one', price: 13000 },
+    { id: 8, name: 'M1 chip not chip LapTop', price: 13000 }
+];
 function matchedProducts(products, search) {
-
+    let matched = [];
+    for (const product of products) {
+        if (product.name.toLowerCase().includes(search.toLowerCase())) {
+            matched.push(product);
+        }
+    }
+    return matched;
 }
+const result = matchedProducts(products, 'phone');
+console.log(result);
